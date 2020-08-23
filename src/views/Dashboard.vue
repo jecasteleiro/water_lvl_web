f8c9477f795b476d625dbaa01c06119ec13cd798
unknown <jecasteleiro@gmail.com>
2020-08-23 15:00:45 -0400

    initial

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-card>lalala</v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'dashboard',
+  components: {
+    
+  }
+}
+</script>
